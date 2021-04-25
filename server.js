f1c1476b6179ce13e0af91f1b0a87d3c381fc00d
akerr501 <58118891+akerr501@users.noreply.github.com>
2021-04-25 12:54:33 -0700

    Prepopulating some data

--- server.js
+++ server.js
@@ -20,6 +20,40 @@ var db = require('./connector'); // connect database
 /*
     ROUTES
 */
+
+var spices = [
+  {"SpiceID": 0, "SpiceName": "Paprika", "SpiceDescription": "Spicy"},
+  {"SpiceID": 1, "SpiceName": "Curry Powder", "SpiceDescription": "smells like curry"}
+];
+var blends = [
+  {"BlendID": 0, "BlendName": "Spicy Blend", "Quantity": 2, "BlendDescription": "very spicy"},
+  {"BlendID": 1, "BlendName": "not so Spicy Blend", "Quantity": 2, "BlendDescription": "not very spicy"}
+];
+var households = [
+  {"HouseholdID": 0, "AddressStreet": "idk", "AddressCity": "idk", "AddressState": "idk", "AddressZip": "idk", "CreationDate": "idk", "UserName": "yessirski", "Password": "1234"},
+  {"HouseholdID": 1, "AddressStreet": "idk", "AddressCity": "idk", "AddressState": "idk", "AddressZip": "idk", "CreationDate": "idk", "UserName": "yessirski2", "Password": "1234"},
+];
+var members = [
+  {"MemberID": 0, "FirstName": "Adam", "MiddleName": "Christopher", "LastName": "Kerr", "HouseholdID": 0}
+  {"MemberID": 1, "FirstName": "Santosh", "LastName": "Ramesh", "HouseholdID": 0}
+];
+var spice_blends = [
+  {"BlendID": 0, "SpiceID": 0},
+  {"BlendID": 0, "SpiceID": 1},
+  {"BlendID": 1, "SpiceID": 0},
+  {"BlendID": 1, "SpiceID": 1}
+];
+var household_following = [
+  {"HouseholdID_1": 0, "HouseholdID_2": 1},
+  {"HouseholdID_1": 1, "HouseholdID_2": 0}
+];
+var household_blends = [
+  {"HouseholdID": 0, "BlendID": 0},
+  {"HouseholdID": 0, "BlendID": 1},
+  {"HouseholdID": 1, "BlendID": 1},
+];
+
+
 // Default route, index/home page of the website
 app.get('/', function(req, res, next) {
   console.log("Serving the Home Page");
@@ -33,7 +67,9 @@ app.get('/households', function(req, res, next) {
   console.log("Serving the Households Page");
   res.status(200);
   res.render("householdPage", {
-    script: "./households.js"
+    script: "./households.js",
+    households: households,
+    household_following: household_following
   });
 });
 
@@ -42,7 +78,8 @@ app.get('/members', function(req, res, next) {
   console.log("Serving the Members Page");
   res.status(200);
   res.render("memberPage", {
-    script: "./members.js"
+    script: "./members.js",
+    members: members
   });
 });
 
@@ -51,7 +88,10 @@ app.get('/spices', function(req, res, next) {
   console.log("Serving the Spices Page");
   res.status(200);
   res.render("spicePage", {
-    script: "./spices.js"
+    script: "./spices.js",
+    spices: spices,
+    blends: blends,
+    spice_blends: spice_blends
   });
 });
 
@@ -60,7 +100,10 @@ app.get('/blends', function(req, res, next) {
   console.log("Serving the Blends Page");
   res.status(200);
   res.render("blendPage", {
-    script: "./blends.js"
+    script: "./blends.js",
+    blends: blends,
+    households: households,
+    household_blends: household_blends
   });
 });
 
